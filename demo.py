933333923a75d3a222b15edce4c6f254d2ba37ae
Sagar Ahirrao <sah@odoo.com>
2017-04-28 01:25:54 +0530

    [IMP] change inputs to sys.argv for command line inputs

--- demo.py
+++ demo.py
@@ -1,19 +1,21 @@
-import csv, operator
+import csv, operator, sys
 
-csvfile = open('data.csv')
+file = sys.argv[1]
+csvfile = open(file)
 data = []
 readCSV = csv.reader(csvfile, delimiter=',')
 for row in readCSV:
     shop_id = int(row[0])
     price = float(row[1])
     product = str(row[2])
-    data.append([shop_id,price,product])
+    data.append([shop_id, price, product])
 
-ch = raw_input("enter 1st: ")
-ch1 = raw_input("enter 2nd: ")
-s={}
+ch = sys.argv[2]
+ch1 = sys.argv[3]
 d = {}
 d1 = {}
+shop_data = {}
+
 for  i in data:
     if i[2] == ch:
         d[i[0]]=i[1]
@@ -24,9 +26,9 @@ if (d1 and d):
     for i in d:
         for j in d1:
             if i == j :
-                s[i]=d[i]+d1[i]
-    if s:
-        print "shop no",min(s.iteritems(), key=operator.itemgetter(1))[0],"with price",min(s.values())
+                shop_data[i]=d[i]+d1[i]
+    if shop_data:
+        print "shop no",min(shop_data.iteritems(), key=operator.itemgetter(1))[0],"with price",min(shop_data.values())
     else:
         print "none"
 else:
